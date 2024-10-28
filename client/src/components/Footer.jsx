d505e1c87fec5cda5c2e4e0d8e04a71c7fca9a37
Kartikey Sharma <IKARTIKEYSHARMA@GMAIL.COM>
2024-10-28 11:53:08 +0530

    new commits tailwind

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => (
     <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
-      <p className="text-white text-left text-xs">@kryptomastery2022</p>
+    <a href="https://github.com/Hackpakt" className="text-white text-left text-xs underline">Developed By Hackpakt</a>
       <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
